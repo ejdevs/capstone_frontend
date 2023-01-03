17b38c0944741b83a3bd066ba01b3751172a77c4
Evan <evanjdevelopment@gmail.com>
2023-01-02 21:13:53 -0600

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import "./App.css";
+import React from "react";
+import { render } from "react-dom";
+import Highcharts from "highcharts";
+import HighchartsReact from "highcharts-react-official";
+
+const options = {
+  title: {
+    text: "My chart",
+  },
+  series: [
+    {
+      data: [1, 2, 3],
+    },
+  ],
+};
+
+const App = () => (
+  <div>
+    <HighchartsReact highcharts={Highcharts} options={options} />
+  </div>
+);
+
+render(<App />, document.getElementById("root"));
+
+// Initially was missing the export line below and wouldn't render
+export default App;
